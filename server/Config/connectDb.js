6c3476f9c0d39d0f331346659859c4ca1021b259
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2025-01-10 10:34:48 +0700

    Refactor server configuration to remove dotenv dependency

--- server/Config/connectDb.js
+++ server/Config/connectDb.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const dotenv = require("dotenv").config();
 const connectDb = async () => {
     mongoose.connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
