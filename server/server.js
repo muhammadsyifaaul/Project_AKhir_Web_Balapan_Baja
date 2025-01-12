dee5d1b049d0b9266ef1dfa2e381636b2a652057
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2025-01-12 14:20:22 +0700

    Refactor User model to include email field

--- server/server.js
+++ server/server.js
@@ -3,13 +3,25 @@ const cors = require('cors');
 require('dotenv').config(); 
 const bodyParser = require('body-parser');
 const { connectDb } = require('./Config/connectDb');
-
+const expressSession = require('express-session');
+const authRoutes = require('./Routes/authRoutes');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(expressSession({
+    secret: 'yeamplowwnaldshhsdthtorharot',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000
+    }
+}));
+app.use(authRoutes);
 
 connectDb(); 
 
