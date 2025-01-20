3b53fb459b3a7ed0d6cdc60118668d4e01cb4f23
Dimawww <dimasadipsetyawan.com>
2025-01-20 20:09:57 +0700

    Login css

--- client/src/components/Login/Login.jsx
+++ client/src/components/Login/Login.jsx
@@ -1,15 +1,3 @@
-// import './Login.css'
-// export default function Login() {
-//     return (
-//         <div>
-//             <form action="http://localhost:3000/Login" method="post">
-//             <input type="text" name="username" placeholder="Username" />
-//             <input type="password" name="password" placeholder="Password" />
-//             <button type="submit">Login</button>
-//             </form>
-//         </div>
-//     )
-// }
 import './Login.css';
 
 export default function Login() {
