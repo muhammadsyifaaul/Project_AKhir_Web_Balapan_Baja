72eecb5e616a47edcff3f09a5fce924a7ed3f218
muhammadsyifaaul <muhammadsyifaaulj@gmail.com>
2025-01-22 10:33:04 +0700

    Refactor DetailPaket and TambahData components for improved data handling and UI: - Removed unnecessary header elements in DetailPaket component. - Integrated TambahData component into DetailPaketGlobal for better data flow. - Enhanced TambahData component to accept props for displaying detail information, allowing for a more dynamic form based on the context (detail view vs. input view). - Updated form fields in TambahData to conditionally render based on the detail view, improving user experience and accessibility.

--- client/src/components/Paket/TambahData.jsx
+++ client/src/components/Paket/TambahData.jsx
@@ -2,7 +2,21 @@ import React from "react";
 import { useTambahData } from "./logic/useTambahData";
 import "./TambahData.css";
 
-export default function TambahData() {
+export default function TambahData({
+  opdFromDetail,
+  namaPekerjaanFromDetail,
+  mulaiKontrakFromDetail,
+  selesaiKontrakFromDetail,
+  nilaiKontrakFromDetail,
+  jangkaWaktuFromDetail,
+  npwpPenyediaFromDetail,
+  skpFromDetail,
+  namaPenyediaFromDetail,
+  alamatPenyediaFromDetail,
+  jenisFromDetail,
+  tenagaAhliFromDetail,
+  isFromDetail,
+}) {
   const {
     mulaiKontrak,
     setMulaiKontrak,
@@ -26,6 +40,7 @@ export default function TambahData() {
 
   return (
     <div className="container">
+      {isFromDetail ? <h2>Detail Paket</h2> : null}
       <form
         action="http://localhost:5000/TambahDataPaket"
         method="post"
@@ -33,152 +48,282 @@ export default function TambahData() {
       >
         <div className="form-group">
           <label htmlFor="opd">OPD</label>
-          <select name="opd" id="opd">
-            <option value="">Pilih OPD</option>
-            {opd.map((opd) => (
-              <option key={opd._id} value={opd.namaOpd}>
-                {opd.namaOpd}
-              </option>
-            ))}
-          </select>
+          {isFromDetail ? (
+            <input
+              type="text"
+              name="opdFormDetail"
+              id="opdFormDetail"
+              placeholder="Masukkan OPD"
+              value={opdFromDetail}
+              disabled
+            />
+          ) : (
+            <>
+              <select name="opd" id="opd">
+                <option value="">Pilih OPD</option>
+                {opd.map((opd) => (
+                  <option key={opd._id} value={opd.namaOpd}>
+                    {opd.namaOpd}
+                  </option>
+                ))}
+              </select>
+            </>
+          )}
         </div>
 
         <div className="form-group">
           <label htmlFor="namaPekerjaan">Nama Pekerjaan</label>
-          <textarea
-            name="namaPekerjaan"
-            id="namaPekerjaan"
-            placeholder="Masukkan nama pekerjaan"
-            rows="1"
-          ></textarea>
+          {isFromDetail ? (
+            <input
+              type="text"
+              name="namaPekerjaanFormDetail"
+              id="namaPekerjaanFormDetail"
+              placeholder="Masukkan Nama Pekerjaan"
+              value={namaPekerjaanFromDetail}
+              disabled
+            />
+          ) : (
+            <input
+              type="text"
+              name="namaPekerjaan"
+              id="namaPekerjaan"
+              placeholder="Masukkan Nama Pekerjaan"
+              required
+            />
+          )}
         </div>
 
         <div className="form-group">
           <div className="date-inputs">
             <div className="date-group">
               <label>Mulai Kontrak</label>
-              <input
-                type="date"
-                name="mulaiKontrak"
-                id="mulaiKontrak"
-                value={mulaiKontrak}
-                onChange={(e) => setMulaiKontrak(e.target.value)}
-                required
-                placeholder="Tanggal Mulai Kontrak"
-              />
+              {isFromDetail ? (
+                <input
+                  type="text"
+                  name="mulaiKontrakFormDetail"
+                  id="mulaiKontrakFormDetail"
+                  value={mulaiKontrakFromDetail}
+                  disabled
+                />
+              ) : (
+                <input
+                  type="date"
+                  name="mulaiKontrak"
+                  id="mulaiKontrak"
+                  value={mulaiKontrak}
+                  onChange={(e) => setMulaiKontrak(e.target.value)}
+                  required
+                  placeholder="Tanggal Mulai Kontrak"
+                />
+              )}
             </div>
 
             <div className="date-group">
               <label>Selesai Kontrak</label>
-              <input
-                type="date"
-                name="selesaiKontrak"
-                id="selesaiKontrak"
-                value={selesaiKontrak}
-                onChange={(e) => setSelesaiKontrak(e.target.value)}
-                required
-                placeholder="Tanggal Selesai Kontrak"
-              />
+              {isFromDetail ? (
+                <input
+                  type="text"
+                  name="selesaiKontrakFormDetail"
+                  id="selesaiKontrakFormDetail"
+                  value={selesaiKontrakFromDetail}
+                  disabled
+                />
+              ) : (
+                <input
+                  type="date"
+                  name="selesaiKontrak"
+                  id="selesaiKontrak"
+                  value={selesaiKontrak}
+                  onChange={(e) => setSelesaiKontrak(e.target.value)}
+                  required
+                  placeholder="Tanggal Selesai Kontrak"
+                />
+              )}
             </div>
           </div>
         </div>
 
         <div className="form-group">
           <label htmlFor="jangkaWaktu">Jangka Waktu (Hari)</label>
-          <input
-            type="text"
-            name="jangkaWaktu"
-            id="jangkaWaktu"
-            value={jangkaWaktu}
-            readOnly
-          />
+          {isFromDetail ? (
+            <input
+              type="number"
+              name="jangkaWaktuFormDetail"
+              id="jangkaWaktuFormDetail"
+              placeholder="Masukkan Jangka Waktu"
+              value={jangkaWaktuFromDetail}
+              disabled
+            />
+          ) : (
+            <input
+              type="number"
+              name="jangkaWaktu"
+              id="jangkaWaktu"
+              placeholder="Masukkan Jangka Waktu"
+              value={jangkaWaktu}
+              onChange={(e) => handleNilaiKontrak(e.target.value)}
+              required
+            />
+          )}
         </div>
 
         <div className="form-group">
           <label htmlFor="nomorKontrak">Nomor Kontrak</label>
-          <input
-            type="text"
-            name="nomorKontrak"
-            id="nomorKontrak"
-            placeholder="Masukkan nomor kontrak"
-            required
-          />
+          {isFromDetail ? (
+            <input
+              type="text"
+              name="nomorKontrakFormDetail"
+              id="nomorKontrakFormDetail"
+              placeholder="Masukkan Nomor Kontrak"
+              value={mulaiKontrakFromDetail}
+              disabled
+            />
+          ) : (
+            <input
+              type="text"
+              name="nomorKontrak"
+              id="nomorKontrak"
+              placeholder="Masukkan Nomor Kontrak"
+              required
+            />
+          )}
         </div>
 
         <div className="form-group">
           <label htmlFor="npwpPenyedia">NPWP Penyedia</label>
           <div style={{ display: "flex", flex: 1 }}>
-            <input
-              type="text"
-              name="npwpPenyedia"
-              id="npwpPenyedia"
-              value={npwpPenyedia}
-              onChange={(e) => setNpwpPenyedia(e.target.value)}
-              placeholder="Tanpa tanda titik[.] dan tanda strip[-]"
-              required
-            />
-            <button onClick={cekNpwp} type="button">
-              Cek
-            </button>
+            {isFromDetail ? (
+              <input
+                type="text"
+                name="npwpPenyediaFormDetail"
+                id="npwpPenyediaFormDetail"
+                placeholder="Masukkan NPWP Penyedia"
+                value={npwpPenyediaFromDetail}
+                disabled
+              />
+            ) : (
+              <input
+                type="text"
+                name="npwpPenyedia"
+                id="npwpPenyedia"
+                placeholder="Masukkan NPWP Penyedia"
+                value={npwpPenyedia}
+                onChange={(e) => setNpwpPenyedia(e.target.value)}
+                required
+              />
+            )}
+            {isFromDetail ? null : (
+              <button onClick={cekNpwp} type="button">
+                Cek
+              </button>
+            )}
           </div>
-          
         </div>
-        {errorNpwp && (
-            <p className="errMsg">{errorNpwp}</p>
-          )}
+        {errorNpwp && <p className="errMsg">{errorNpwp}</p>}
 
         <div className="form-group">
           <label htmlFor="namaPenyedia">Nama Penyedia</label>
-          <input
-            type="text"
-            name="namaPenyedia"
-            id="namaPenyedia"
-            value={dataPenyedia.nama || ""}
-            readOnly
-          />
+          {isFromDetail ? (
+            <input
+              type="text"
+              name="namaPenyediaFormDetail"
+              id="namaPenyediaFormDetail"
+              placeholder="Masukkan Nama Penyedia"
+              value={namaPenyediaFromDetail}
+              disabled
+            />
+          ) : (
+            <input
+              type="text"
+              name="namaPenyedia"
+              id="namaPenyedia"
+              value={dataPenyedia.nama || ""}
+              readOnly
+            />
+          )}
         </div>
 
         <div className="form-group">
           <label htmlFor="alamatPenyedia">Alamat Penyedia</label>
-          <input
-            type="text"
-            name="alamatPenyedia"
-            id="alamatPenyedia"
-            value={dataPenyedia.alamat || ""}
-            readOnly
-          />
+          {isFromDetail ? (
+            <input
+              type="text"
+              name="alamatPenyediaFormDetail"
+              id="alamatPenyediaFormDetail"
+              placeholder="Masukkan Alamat Penyedia"
+              value={alamatPenyediaFromDetail}
+              disabled
+            />
+          ) : (
+            <input
+              type="text"
+              name="alamatPenyedia"
+              id="alamatPenyedia"
+              value={dataPenyedia.alamat || ""}
+              readOnly
+            />
+          )}
         </div>
 
         <div className="form-group">
-          <label htmlFor="skp">Sisa Kemampuan Paket</label>
-          <input
-            type="number"
-            name="skp"
-            id="skp"
-            placeholder="Masukkan sisa kemampuan paket"
-          />
+
+          {isFromDetail ? (
+            null
+          ) : (
+            <>
+            <label htmlFor="skp">Sisa Kemampuan Paket</label>
+            <input
+              type="number"
+              name="skp"
+              id="skp"
+              placeholder="Masukkan sisa kemampuan paket"
+            />
+            </>
+          )}
         </div>
 
         <div className="form-group">
           <label htmlFor="jenis">Kategori Pekerjaan</label>
-          <select name="jenis" id="jenis">
-            <option value="Kecil">Kecil</option>
-            <option value="Non Kecil">Non Kecil</option>
-          </select>
+          {isFromDetail ? (
+            <input
+              type="text"
+              name="jenis"
+              id="jenis"
+              placeholder="Masukkan kategori pekerjaan"
+              value={jenisFromDetail}
+              disabled
+            />
+          ) : (
+            <select name="jenis" id="jenis">
+              <option value="Kecil">Kecil</option>
+              <option value="Non Kecil">Non Kecil</option>
+            </select>
+          )}
         </div>
 
         <div className="form-group">
           <label htmlFor="nilaiKontrak">Nilai Kontrak</label>
-          <input
-            type="number"
-            name="nilaiKontrak"
-            id="nilaiKontrak"
-            onChange={handleNilaiKontrak}
-            placeholder="Masukkan nilai kontrak"
-          />
+          {isFromDetail ? (
+            <input
+              type="number"
+              name="nilaiKontrak"
+              id="nilaiKontrak"
+              placeholder="Masukkan nilai kontrak"
+              value={nilaiKontrakFromDetail}
+              disabled
+            />
+          ) : (
+            <input
+              type="number"
+              name="nilaiKontrak"
+              id="nilaiKontrak"
+              onChange={handleNilaiKontrak}
+              placeholder="Masukkan nilai kontrak"
+            />
+          )}
         </div>
 
-        {nilaiKontrak >= 200000000 && (
+        {/* {nilaiKontrak >= 200000000 && (
           <div className="form-group">
             <label htmlFor="tenagaAhli">Tenaga Ahli</label>
             <div style={{ display: "flex", flex: 1 }}>
@@ -201,13 +346,57 @@ export default function TambahData() {
               </button>
             </div>
           </div>
+        )} */}
+        {isFromDetail ? (
+          <div className="form-group">
+            <label htmlFor="tenagaAhli">Tenaga Ahli</label>
+            <input
+              type="text"
+              name="tenagaAhli"
+              id="tenagaAhli"
+              placeholder="NPWP atau Nama"
+              value={tenagaAhliFromDetail}
+              disabled
+            />
+          </div>
+        ) : (
+          nilaiKontrak >= 200000000 && (
+            <div className="form-group">
+              <label htmlFor="tenagaAhli">Tenaga Ahli</label>
+              <div style={{ display: "flex", flex: 1 }}>
+                <input
+                  type="text"
+                  name="tenagaAhli"
+                  id="tenagaAhli"
+                  placeholder="NPWP atau Nama"
+                  onChange={(e) => handleInputTenagaAhli(e.target.value)}
+                  value={inputTenagaAhli}
+                />
+                <input
+                  type="hidden"
+                  name="idTenagaAhli"
+                  id="idTenagaAhli"
+                  value={idTenagaAhli}
+                />
+                <button onClick={cekTenagaAhli} type="button">
+                  Cek
+                </button>
+              </div>
+            </div>
+          )
         )}
 
         <div className="button-group">
-          <button type="reset" onClick={handleResetState}>
-            Reset
-          </button>
-          <button type="submit">Tambah Data</button>
+          {isFromDetail ? (
+            <button type="button">Kembali</button>
+          ) : (
+            <>
+              <button type="reset" onClick={handleResetState}>
+                Reset
+              </button>
+              <button type="submit">Tambah Data</button>
+            </>
+          )}
         </div>
       </form>
     </div>
